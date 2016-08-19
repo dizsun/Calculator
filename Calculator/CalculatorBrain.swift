aa154b0d527a5dab097f078b767ced65f4dc5f95
dizsun <dizsun@126.com>
2016-08-19 17:05:32 +0800

    Changed the algorithm

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -14,10 +14,10 @@ import Foundation
 class CalculatorBrain {
     //MARK: 内置数据
     //计算器的输入有三类：数字、一元运算符、二元运算符
-    private enum Op: CustomStringConvertible {
+    private enum Operation: CustomStringConvertible {
         case Operand(Double)    //运算数字
-        case UnaryOperation(String,Double->Double)  //一元运算符
-        case BinaryOperation(String,(Double,Double)->Double)    //二元运算符
+        case UnaryOperator(String,Double->Double)  //一元运算符
+        case BinaryOperator(String,(Double,Double)->Double)    //二元运算符
         case Dot
         case Parenthese(Bool)//括号，如果Parenthese的值是true则为)否则为(
         case PI
@@ -28,9 +28,9 @@ class CalculatorBrain {
                 switch self{
                 case .Operand(let operand):
                     return "\(operand)"
-                case .UnaryOperation(let operation, _):
+                case .UnaryOperator(let operation, _):
                     return "\(operation)"
-                case .BinaryOperation(let operation, _):
+                case .BinaryOperator(let operation, _):
                     return "\(operation)"
                 case .Dot:
                     return "."
@@ -49,7 +49,7 @@ class CalculatorBrain {
             }
         }
         //运算符的比较，+，-运算优先级最小为0，*，/为1，^为2，单目运算符为3
-        func compareTo(other:Op) -> Int {
+        func compareTo(other:Operation) -> Int {
             let priority = [ "+":0, "−":0, "×":1, "÷":1, "^":2, "√":3, "log":3, "ln":3, "sin":3, "cos":3, "tan":3 ]
             if priority.keys.contains(self.description)
                 && priority.keys.contains(other.description) {
@@ -59,27 +59,27 @@ class CalculatorBrain {
         }
     }
     //盛放输入内容的栈
-    private var opStack = [Op]()
+    private var preStack = [Operation]()
     //所有会用到的运算符集合
-    private var knownOps = [String:Op]()
+    private var knownOps = [String:Operation]()
     
     init(){
-        knownOps["+"] = Op.BinaryOperation("+", +)
-        knownOps["−"] = Op.BinaryOperation("−", -)
-        knownOps["×"] = Op.BinaryOperation("×", *)
-        knownOps["÷"] = Op.BinaryOperation("÷", /)
-        knownOps["^"] = Op.BinaryOperation("^", pow)
-        knownOps["√"] = Op.UnaryOperation("√", sqrt)
-        knownOps["log"] = Op.UnaryOperation("log", log2)
-        knownOps["ln"] = Op.UnaryOperation("ln", log)
-        knownOps["sin"] = Op.UnaryOperation("sin", sin)
-        knownOps["cos"] = Op.UnaryOperation("cos", cos)
-        knownOps["tan"] = Op.UnaryOperation("tan", tan)
-        knownOps["("] = Op.Parenthese(false)
-        knownOps[")"] = Op.Parenthese(true)
-        knownOps["."] = Op.Dot
-        knownOps["e"] = Op.E
-        knownOps["pi"] = Op.PI
+        knownOps["+"] = Operation.BinaryOperator("+", +)
+        knownOps["−"] = Operation.BinaryOperator("−", -)
+        knownOps["×"] = Operation.BinaryOperator("×", *)
+        knownOps["÷"] = Operation.BinaryOperator("÷", /)
+        knownOps["^"] = Operation.BinaryOperator("^", pow)
+        knownOps["√"] = Operation.UnaryOperator("√", sqrt)
+        knownOps["log"] = Operation.UnaryOperator("log", log2)
+        knownOps["ln"] = Operation.UnaryOperator("ln", log)
+        knownOps["sin"] = Operation.UnaryOperator("sin", sin)
+        knownOps["cos"] = Operation.UnaryOperator("cos", cos)
+        knownOps["tan"] = Operation.UnaryOperator("tan", tan)
+        knownOps["("] = Operation.Parenthese(false)
+        knownOps[")"] = Operation.Parenthese(true)
+        knownOps["."] = Operation.Dot
+        knownOps["e"] = Operation.E
+        knownOps["pi"] = Operation.PI
     }
     //MARK: 接口函数
     //计算算式值
@@ -87,21 +87,25 @@ class CalculatorBrain {
         if !parseFormula() {
             return nil
         }
-        return evaluate(opStack)
+        //        print(preStack)
+        preToIn()
+        //        print(inStack)
+        return Double(calculateIn()!)
     }
     //将运算数字或运算符压入栈中
     func pushOperand(operand: String){
         if knownOps.keys.contains(operand) {
-            opStack.append(knownOps[operand]!)
+            preStack.append(knownOps[operand]!)
         }
         else{
-            opStack.append(Op.Operand(Double(operand)!))
+            preStack.append(Operation.Operand(Double(operand)!))
         }
     }
+
     //将输入的数字或运算符删除一个
     func popOperand() -> String {
-        if opStack.count > 0 {
-            if let value = opStack.popLast(){
+        if preStack.count > 0 {
+            if let value = preStack.popLast(){
                 return value.description
             }
             else{
@@ -112,137 +116,23 @@ class CalculatorBrain {
     }
     //将输入的内容清零
     func clearOpSatck(){
-        opStack.removeAll()
+        preStack.removeAll()
     }
     //MARK: 运算逻辑
-    //将括号完全去除得到简单算式
-    private func evaluate(ops:[Op]) -> Double? {
-        if ops.isEmpty {
-            return 0
-        }
-        var hasParenthese = false //算式中是否含有括号
-        label: for op in ops {
-            switch op {
-            case .Parenthese(_):
-                hasParenthese = true
-                break label
-            default:break
-            }
-        }
-        if hasParenthese {
-            var tempStack = [Op]()
-            for var i=0; i<ops.count; i++ {
-                switch ops[i] {
-                case .Parenthese(_):
-                    guard let rightIndex = findMatchingParenthese(ops, index: i) else{
-                        return nil
-                    }
-                    guard let innnerFormula = getFormulaInRange(ops, preIndex: i, postIndex: rightIndex) else {
-                        return nil
-                    }
-                    i = rightIndex
-                    guard let innnerValue = evaluate(innnerFormula) else{
-                        return nil
-                    }
-                    tempStack.append(Op.Operand(innnerValue))
-                default: tempStack.append(ops[i])
-                }
-            }
-            return evaluate(tempStack)
-        }
-        else{
-            return evaluateSimpleFormula(ops)
-        }
-    }
-    //计算简单算式
-    private func evaluateSimpleFormula(ops:[Op]) -> Double? {
-        let rightStack = middleToPost(ops)
-        return evaluateRightFormula(rightStack)
-    }
-    //将中序表达式变换为后序表达式
-    private func middleToPost(initialSatck: [Op]) -> [Op] {
-        var postStack = [Op]()
-        var tempStack = [Op]()
-        for var eachOp in initialSatck {
-            switch eachOp {
-            case .Operand(_):
-                postStack.append(eachOp)
-            case .Parenthese(let direction):
-                if !direction {
-                    tempStack.append(eachOp)
-                }
-                else{
-                    var tempValue = tempStack.removeLast()
-                    var value = tempValue.description
-                    while value != Op.Parenthese(true).description {
-                        postStack.append(tempValue)
-                        tempValue = tempStack.removeLast()
-                        value = tempValue.description
-                    }
-                }
-            case .BinaryOperation(_, _):
-                if tempStack.isEmpty {
-                    tempStack.append(eachOp)
-                }
-                else{
-                    if eachOp.compareTo(tempStack.last!) > 0 {
-                        tempStack.append(eachOp)
-                    }
-                    else{
-                        while !tempStack.isEmpty && eachOp.compareTo(tempStack.last!) <= 0 {
-                            postStack.append(tempStack.removeLast())
-                        }
-                        tempStack.append(eachOp)
-                    }
-                }
-            case .UnaryOperation(_, _):
-                tempStack.append(eachOp)
-            default:
-                break
-            }
-        }
-        while !tempStack.isEmpty {
-            postStack.append(tempStack.removeLast())
-        }
-        return postStack
-    }
-    //计算后序表达式
-    private func evaluateRightFormula(rightStack: [Op]) -> Double?{
-        var tempStack = [Double]()
-        for var i=0; i<rightStack.count; i++ {
-            switch rightStack[i] {
-            case .Operand(let value):
-                tempStack.append(value)
-            case .BinaryOperation(_, let perform):
-                let rightValue = tempStack.removeLast()
-                let leftValue = tempStack.removeLast()
-                let result = perform(leftValue,rightValue)
-                tempStack.append(result)
-            case .UnaryOperation(_, let perform):
-                let result = perform(tempStack.removeLast())
-                tempStack.append(result)
-            default: break
-            }
-        }
-        if tempStack.isEmpty {
-            return nil
-        }
-        else{
-            return tempStack.removeLast()
-        }
-    }
-    //将opStack中的分离的数字解析为一个数字
+    //中缀表达式
+    private var inStack = [Operation]()
+    //算式解析，将数字拼在一起，将E与PI替换成数字
     private func parseFormula() -> Bool {
         //首先判断括号是否一一匹配
         var parentsis = 0
-        for var i=0; i<opStack.count; i++ {
-            switch opStack[i] {
+        for op in preStack {
+            switch op {
             case .Parenthese(let direction):
                 if direction {
-                    parentsis--
+                    parentsis=parentsis-1
                 }
                 else{
-                    parentsis++
+                    parentsis=parentsis+1
                 }
             default:break
             }
@@ -251,10 +141,10 @@ class CalculatorBrain {
             return false
         }
         //接着解析数字
-        var tempStack = [Op]()
+        var tempStack = [Operation]()
         var buffer:String = ""
-        for var i=0;i<opStack.count;i++ {
-            switch opStack[i] {
+        for op in preStack {
+            switch op {
             case .Operand(let value):
                 buffer += "\(Int(value))"
                 continue
@@ -262,53 +152,120 @@ class CalculatorBrain {
                 buffer += "."
                 continue
             case .E:
-                buffer = "\(M_E)"
+                tempStack.append(Operation.Operand(M_E))
             case .PI:
-                buffer = "\(M_PI)"
+                tempStack.append(Operation.Operand(M_PI))
             default:
                 if !buffer.isEmpty{
                     if let value = Double(buffer) {
-                        tempStack.append(Op.Operand(value))
+                        tempStack.append(Operation.Operand(value))
                         buffer = ""
                     }
                     else {
                         return false
                     }
                 }
-                tempStack.append(opStack[i])
+                tempStack.append(op)
             }
         }
         if let value = Double(buffer) {
-            tempStack.append(Op.Operand(value))
+            tempStack.append(Operation.Operand(value))
         }
-        opStack = tempStack
+        preStack = tempStack
         return true
     }
-    //找到与左括号匹配的那个右括号的索引,若不存在则返回nil
-    private func findMatchingParenthese(ops:[Op], index: Int) -> Int? {
-        var count = 1
-        for var i = index+1; i<ops.count; i++ {
-            switch ops[i] {
-            case .Parenthese(false): count++
-            case .Parenthese(true): count--
-            default: break
+    
+    
+    //前缀表达式转中缀表达式
+    private func preToIn(){
+        var temp = [Operation]()
+        var unaryOperator:Operation?
+        for op in preStack {
+            //            print(op)
+            switch op {
+            case .Operand(_):
+                inStack.append(op)
+                if unaryOperator != nil {
+                    inStack.append(unaryOperator!)
+                    unaryOperator=nil
+                }
+                
+            case .UnaryOperator(_, _):
+                unaryOperator = op
+            case .BinaryOperator(_, _):
+                if !temp.isEmpty {
+                    lable: while temp.last!.compareTo(op)>=0 {
+                        switch temp.last! {
+                        case .Parenthese(false):
+                            break lable
+                        default:
+                            inStack.append(temp.removeLast())
+                        }
+                        if temp.isEmpty {
+                            break
+                        }
+                    }
+                }
+                temp.append(op)
+            case .Parenthese(false):
+                temp.append(op)
+            case .Parenthese(true):
+                lable: while true {
+                    switch temp.last! {
+                    case .Parenthese(false):
+                        temp.removeLast()
+                        break lable
+                    default:
+                        inStack.append(temp.removeLast())
+                    }
+                }
+            default:
+                break
             }
-            if count == 0 {
-                return i
+            //            print(temp)
+            //            print(inStack)
+        }
+        if !temp.isEmpty {
+            while true {
+                if let _ = temp.last {
+                    inStack.append(temp.removeLast())
+                }
+                else{
+                    break
+                }
+                if !temp.isEmpty {
+                    break
+                }
             }
         }
-        return nil
     }
-    //得到括号内的算式，左右索引均为开区间，如果括号内没有值则返回0，如果算式有误返回nil
-    private func getFormulaInRange(ops:[Op], preIndex:Int, postIndex: Int) -> [Op]?{
-        if preIndex >= postIndex+1 {
+    
+    //计算中缀表达式
+    private func calculateIn() -> String?{
+        var operandStack = [Double]()
+        for op in inStack {
+            switch op {
+            case .Operand(let value):
+                operandStack.append(value)
+            case .UnaryOperator(_, let unaryOperator):
+                operandStack.append(unaryOperator(operandStack.removeLast()))
+            case .BinaryOperator(_, let binaryOperator):
+                let rightValue = operandStack.removeLast()
+                let leftValue = operandStack.removeLast()
+                operandStack.append(binaryOperator(leftValue,rightValue))
+            default:
+                break
+            }
+            //            print(op)
+            //            print(operandStack)
+        }
+        if operandStack.isEmpty {
+            //            print("is Empty")
             return nil
         }
-        var tempStack = [Op]()
-        for var i=preIndex+1; i<postIndex; i++ {
-            tempStack.append(ops[i])
+        else{
+            return operandStack.last?.description
         }
-        return tempStack
     }
 
 }
